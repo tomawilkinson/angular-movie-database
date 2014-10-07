46c69207d4bed2678be3222c64063f9434b67c83
Thomas <tom@dosedigital.com>
2014-10-07 22:17:26 +0100

    Connect to Rotten Tomatoes API

--- js/main.js
+++ js/main.js
@@ -17,3 +17,17 @@ angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope)
 		});
 	};
 });
+
+var apiKey = 'uyha8h3zge33dghf8bpkjnyp';
+var searchTerm = encodeURIComponent('Toy Story');
+var pageLimit = 4;
+var pageNumber = 1;
+var url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey='+apiKey+'&q='+searchTerm+'&page_limit='+pageLimit+'&page='+pageNumber;
+
+$.ajax({
+	url: url,
+	dataType: "jsonp",
+	success: function(response) {
+		console.log(response);
+	}
+});
