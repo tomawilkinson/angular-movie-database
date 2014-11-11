9ca2f8f5502af7ad1c6626eef91a38ea8c2ed5f5
Thomas <tom@dosedigital.com>
2014-11-11 00:26:39 +0000

    Extended pagination - amended page limit to 25 as per the API - Added broken loading functionality

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 
-
+// Init angular app
 var movieApp = angular.module('movieApp', ['ui.bootstrap']);
 
 
+// Return Rotten Tomato movie data - AJAX
 movieApp.factory('requestMovieData', function($http) {
 
 	return {
-		getMovieData: function(pageLimit, pageNumber) {
+		getMovieData: function(pageLimit, pageNumber, searchValue) {
 
+			// Vars
 			var apiKey = 'uyha8h3zge33dghf8bpkjnyp';
-			var searchTerm = 'the';
+			var searchValue = searchValue;
 			var pageLimit = pageLimit;
 			var pageNumber = pageNumber;
 			var domain = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=';
-			var params = apiKey+'&q='+searchTerm+'&page_limit='+pageLimit+'&page='+pageNumber;
+			var params = apiKey+'&q='+searchValue+'&page_limit='+pageLimit+'&page='+pageNumber;
 			var url = domain + params;
 
 			return $http.get(url).then(function(result) {
 				return result.data;
 			});
+
 		}
 	}
 
@@ -26,40 +29,38 @@ movieApp.factory('requestMovieData', function($http) {
 
 movieApp.controller('searchCtrl', function($scope, requestMovieData){
 
-	// $scope.pageLimit = 5;
-	// $scope.pageNumber = 1;
-
-	$scope.maxSize = 8;
-
+	// Vars
+	$scope.maxSize = 5;
+	$scope.resultsTotal = null;
+	$scope.pageTotal = null;
 	$scope.pageNumber = 1;
+	$scope.loading = true;
 
-	$scope.resultsTotal = 100;
-	$scope.pageTotal = 100;
+	$scope.loadMovies = function() {
 
-	var getStuff = function() {
+		$scope.pageLimit = 10;
 
-		$scope.pageLimit = 3;
-
-		//console.log($scope.pageNumber)
-
-		requestMovieData.getMovieData($scope.pageLimit, $scope.pageNumber).then(function(movieData) {
+		requestMovieData.getMovieData($scope.pageLimit, $scope.pageNumber, $scope.searchValue).then(function(movieData) {
+			$scope.loading = false;
 			$scope.movieData = movieData;
 			$scope.movies = $scope.movieData['movies'];
 			$scope.resultsTotal = $scope.movieData['total'];
-			//console.log($scope.movieData)
 		});
+
 	}
 
-	getStuff();
-
 	$scope.pageChanged = function() {
-		$scope.pageNumber = $scope.currentPage;
-		getStuff();
+		$scope.loadMovies();
 	};
 
 	$scope.$watch('resultsTotal + pageNumber', function() {
-		$scope.pageTotal = ($scope.resultsTotal / $scope.pageLimit);
-		//console.log()
+		// API has 25 page limit
+		// Need a smarter way of calculating page limit to max 25 - needs to be dynamic based on pageLimit
+		if($scope.resultsTotal / $scope.pageLimit >= 250) {
+			$scope.pageTotal = 250;
+		}else {
+			$scope.pageTotal = ($scope.resultsTotal / $scope.pageLimit);
+		}
 	});
 
 });
